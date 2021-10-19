17bfd21543b84e991d71f9a0f4e142e8a8946b14
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-19 06:37:10 +0100

    Initial Commit

---
+++ Shared/Swift_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Card_GameApp.swift
+//  Shared
+//
+//  Created by Vijayaraghavan Sundararaman on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
