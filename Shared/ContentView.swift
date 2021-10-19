a65468d2c0ab35dbe7aa27377da1b8a22df7d083
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-19 07:20:44 +0100

    Initial Commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            Image("background").ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Image("dealbutton")
+                
+                Spacer()
+                
+                HStack{
+                
+                    Spacer()
+                    
+                    VStack{
+                        Text("Player")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding(.bottom)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack{
+                        Text("CPU")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding(.bottom)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+        }
+       
     }
 }
 
